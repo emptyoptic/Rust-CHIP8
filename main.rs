a695094f1693e116c0694b2b4ec8d59c1e52b759
S.DEV <56833032+emptyoptic@users.noreply.github.com>
2023-12-04 19:28:04 +0100

    Finally got thing moving

--- main.rs
+++ main.rs
@@ -1,65 +1,81 @@
 use std::fs::File;
 use std::io::Read;
+// TODO: Once basic funtionality is implemented add SDL for drawing
+
+// https://multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
 
 #[derive(Debug)]
+#[allow(dead_code)] // Will be removed once all is used
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
 struct Chip8_cpu {
-    opcode: u16,            // Operation calls
-    memory: [u8; 4096],     // 4KB = 4096 bytes
-    V: [u8; 16],            // Registers: V0 .. V16
-    I: u16,                 // Index register
-    pc: u16,                // Program counter
-    gfx: [u8; 64*32],       // Graphics = 2048 pixels
-    delay_timer: u8,        // 60Hz counter
-    sound_timer: u8,        // 60HZ counter
-    stack: [u16; 16],       // The call stack
-    sp: u16,                // The stack pointer remembers the current location
-    key: [u8; 16],          // Keyboard input
-    fontset: [u8; 80]       // Default fontset
+    opcode: u16,        // Operation calls
+    memory: [u8; 4096], // 4KB = 4096 bytes
+    V: [u8; 16],        // Registers: V0 .. V16
+    I: u16,             // Index register
+    pc: u16,            // Program counter
+    gfx: [u8; 64 * 32], // Graphics = 2048 pixels
+    delay_timer: u8,    // 60Hz counter
+    sound_timer: u8,    // 60HZ counter
+    stack: [u16; 16],   // The call stack
+    sp: u16,            // The stack pointer remembers the current location
+    key: [u8; 16],      // Keyboard input
+    fontset: [u8; 80],  // Default fontset
 }
 
 // initialize the Chip8 and set initial values to zero
 impl Default for Chip8_cpu {
     fn default() -> Self {
         Self {
-            opcode: 0,            
-            memory: [0; 4096],     
-            V: [0; 16],            
-            I: 0,                 
-            pc: 0,                
-            gfx: [0; 64*32],       
-            delay_timer: 0,        
-            sound_timer: 0,        
-            stack: [0; 16],       
-            sp: 0,                
+            opcode: 0,
+            memory: [0; 4096],
+            V: [0; 16],
+            I: 0,
+            pc: 0,
+            gfx: [0; 64 * 32],
+            delay_timer: 0,
+            sound_timer: 0,
+            stack: [0; 16],
+            sp: 0,
             key: [0; 16],
-            fontset: [0; 80]
+            fontset: [0; 80],
         }
     }
 }
 
-fn open_rom(c8: &mut Chip8_cpu, file_name: &str) {
-    let mut file_buffer = File::open(file_name).unwrap();           // Open file
-    let mut data = Vec::<u8>::new();                                // Use vector type array for the data
-    file_buffer.read_to_end(&mut data);                             // Read the whole file and store data in 8 bits
+fn emulate(c8: &mut Chip8_cpu) {
+    c8.opcode = ((c8.memory[c8.pc as usize] as usize) << 8
+        | (c8.memory[(c8.pc as usize) + (1 as usize)] as usize)) as u16; // Retrieve opcodes from file
 
-    for i in 0..data.len() {
-        c8.memory[(c8.pc as usize) + i] = data[i];
+    match (c8.opcode) & (0xF000) {
+        // ANNN: Set I to the address NNN
+        0xA000 => {
+            c8.I = c8.opcode & 0x0FFF; // Executing the opcode
+            c8.pc += 2;
+        }
+        _ => println!("Unknow opcode: 0x{}", c8.opcode),
     }
-
-    //TODO: Convert file to opcodes (instructions)
 }
 
-// TODO: Add update function
-// TODO: Add render (draw) function
+#[allow(unused_must_use)]
+fn open_rom(c8: &mut Chip8_cpu, file_name: &str) {
+    let mut file_buffer = File::open(file_name).unwrap(); // Open file
+    let mut data = Vec::<u8>::new(); // Use vector type array for the data
+    file_buffer.read_to_end(&mut data); // Read the whole file and store data in 8 bits
 
+    for i in 0..data.len() {
+        c8.memory[(c8.pc as usize) + i] = data[i]; // Load file data into memory
+    }
+
+    // TODO: Convert file to opcodes (instructions)
+}
 
 fn init(c8: &mut Chip8_cpu) {
-    c8.pc = 0x200;          // Program counter starts at memory location 512
-    c8.opcode = 0x0;        // Set initial opcode to zero
-    c8.I = 0x0;             // Set initial index to zero
-    c8.sp = 0x0;            // Set the stack pointer to zero
-    c8.fontset =
-    [
+    c8.pc = 0x200; // Program counter starts at memory location 512
+    c8.opcode = 0x0; // Set initial opcode value to be zero
+    c8.I = 0x0; // Set initial index value to be zero
+    c8.sp = 0x0; // Set the stack pointer vlaue to be zero
+    c8.fontset = [
         0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
         0x20, 0x60, 0x20, 0x20, 0x70, // 1
         0xf0, 0x10, 0xf0, 0x80, 0xf0, // 2
@@ -75,19 +91,24 @@ fn init(c8: &mut Chip8_cpu) {
         0xF0, 0x80, 0x80, 0x80, 0xF0, // C
         0xE0, 0x90, 0x90, 0x90, 0xE0, // D
         0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-        0xF0, 0x80, 0xF0, 0x80, 0x80  // F  
+        0xF0, 0x80, 0xF0, 0x80, 0x80, // F
     ];
-    
+
     for i in 0..80 {
         c8.memory[i] = c8.fontset[i]; // Load fontset into memory
     }
 }
 
+// TODO: Add update function
+// TODO: Add render (draw) function
+
 fn main() {
     let mut chip8 = Chip8_cpu::default();
-    
+
     init(&mut chip8);
     open_rom(&mut chip8, "IBM Logo.ch8");
-    
-    println!("{chip8:#?}"); 
+    // emulate(&mut chip8);
+    // TODO: Add a main loop
+
+    println!("{chip8:#?}");
 }
